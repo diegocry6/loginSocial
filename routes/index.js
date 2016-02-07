b5fbb92f99a65351aae8fbeb5cf2bee244a02447
Diego <makidiegoaerox@gmail.com>
2016-02-07 19:59:56 +0100

    Listo Login y Control de Errores

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var sqlite3 = require('sqlite3').verbose();
+db = new sqlite3.Database('loginsocial.db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,5 +11,25 @@ router.get('/', function(req, res, next) {
 router.get('/loged', function(req, res, next) {
   res.render('loged', { title: 'LoginSocial' });
 });
+
+router.post('/', function(req, res, next) {
+    console.log(req.body);
+
+      stmt = db.prepare("SELECT * FROM usuarios WHERE username = ? AND password = ?");
+          stmt.bind(req.body.username, req.body.password);
+          stmt.get(function(error, row)
+          {
+            if ((req.body.username) || (req.body.password)) {
+              if (row) {
+                res.redirect('/loged');
+              } else {
+                res.send("Error en las credenciales <a href='/'>Volver</a>", 200);
+              }
+            } else {
+                res.send("Los campos no pueden estar vacios <a href='/'>Volver</a>", 200);
+            }
+          });
+});
+
 
 module.exports = router;
